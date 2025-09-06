11d0e9a78a4e6cfa8707b3d2a8cb5d750432e6e6
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-06 19:47:58 +0100

    add loader for retrieving token data (verified backend), passing to Outlet context along with socket connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
-import { useEffect } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useLoaderData } from 'react-router-dom'
+import { io } from 'socket.io-client'
 
-
+const token = localStorage.getItem("token")
+const socket = io('http://localhost:3001/profile', {
+  autoConnect: false, // manually called in Profile
+  auth: { 
+    token: token,
+  }
+})
+  
 const App = () => {
+
+  const [userState, setUserState] = useState()
+  const user = useLoaderData()
+  
+  useEffect(() => {
+    setUserState(user)
+  }, [user])
+
+  console.log("app ID: ", socket.id)
 
   return (
     <>
     <h1>Chat App</h1>
-    <Link to="profile">Profile</Link>
-    <Outlet />
+    <Link to="profile">Profile</Link><br />
+    <Link to="login">Login</Link><br />
+    <Link to="sign-up">Signup</Link><br />
+    <Link to="rooms/:id">Rooms</Link>
+    <Outlet context={ [socket, userState, setUserState] }/>
     </>
   )
 
