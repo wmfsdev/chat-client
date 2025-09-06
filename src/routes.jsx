20019115dd53c4650611002585cda3b0a0ac4221
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-06 19:50:22 +0100

    import appLoader to loader for app root path

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,19 +2,25 @@ import App from "./App";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Rooms from "./components/Rooms";
+import Messages from "./components/Messages";
 import Room from "./components/Room";
+import { appLoader } from "./util/loader";
 
 const routes = [
   {
     path: '/',
+    loader: appLoader,
     element: <App/>,
     children: [
+      { path: '/rooms/:id', element: <Room /> },
       { path: '/sign-up', element: <Signup /> },
       { path: '/login', element: <Login /> },
       { path: '/profile',
         element: <Profile />,
         children: [
-          { path: '/profile/room', element: <Room /> }
+          { path: '', element: <Rooms /> },
+          { path: '/profile/messages', element: <Messages /> }
         ] 
       }
     ]
