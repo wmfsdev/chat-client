43761dbaf1b39ac5d5764c44833dc0d0b0dd523d
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-11 23:50:04 +0100

    create component for handling one-to-one communication between users

---
+++ src/components/Chat.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+
+const Chat = ({ recipientInfo, socket, username }) => {
+
+  const [message, setMessage] = useState('')
+  const [chat, setChat] = useState([])
+  const uniqueConversationID = recipientInfo.recipientId
+  
+  function sendMessage() {
+    console.log("sending message")
+    const id = crypto.randomUUID()
+    socket.emit("send_priv_message", { from: { socketId: socket.id, username: username }, to: recipientInfo.recipientId, message: message, id: id })
+    setChat([...chat, { id: id, username: username, message: message }])
+  }
+  
+  useEffect(() => { // RECEIVE PRIVATE MESSAGE
+    socket.on("receive_priv_message", (data) => {
+      if (data.from.socketId !== uniqueConversationID) {
+        console.log("non-message")
+      } else {
+        setChat([...chat, { id: data.id, username: data.from.username, message: data.message }])
+      }
+    })
+    return () => {
+      socket.off("receive_priv_message")
+    }
+  })
+  
+  return (
+    <>
+    <h2>PRIVATE CHAT</h2>
+    <p>Chatting with {recipientInfo.recipientUsername}</p>
+    <ul>
+      {chat.map(data => (
+        <li key={data.id}>{data.username}: {data.message}</li>
+      ))}
+    </ul> 
+    <div className="chat-type">
+      <input type="text" placeholder="message" onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={sendMessage}>send</button>
+    </div>
+    </>
+  )
+}
+
+export default Chat
