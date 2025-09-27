a892bd3ef34ab81b2b66ece48ffce47c3bd3df45
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-27 23:56:24 +0100

    create useEffect for retrieving chat history

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,20 +4,55 @@ const Chat = ({ recipientInfo, socket, sender }) => {
 
   const [message, setMessage] = useState('')
   const [chat, setChat] = useState([])
-  const uniqueConversationID = recipientInfo.recipientId
+  const conversationId = recipientInfo.recipientId
   
   function sendMessage() {
     console.log("sending message")
     const id = crypto.randomUUID()
-    socket.emit("send_priv_message", { from: { id: sender.userId, username: sender.username }, to: recipientInfo.recipientId, message: message, id: id })
+    socket.emit("send_priv_message", 
+      { 
+        from: { id: sender.userId, username: sender.username }, 
+        to: conversationId, 
+        message: message, 
+        id: id
+      })
     setChat([...chat, { id: id, username: sender.username, message: message }])
   }
+
+  useEffect(() => { // RETRIEVE CHAT HISTORY
+    async function getChat() {
+      try {
+        const response = await fetch(`http://localhost:3001/chat`, { 
+          method: "POST",
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+          },
+          body: JSON.stringify({
+            room: conversationId,
+            author: sender.userId
+          }) 
+        })
+        const data = await response.json()
+        const chat = data.map((d) => {
+          return { id: d.id, username: d.author.username, message: d.content }
+        })
+        setChat(chat)
+      } catch(err) {
+        return err
+      }
+    }
+    getChat()
+  }, [conversationId, sender.userId])
   
   useEffect(() => { // RECEIVE PRIVATE MESSAGE
     socket.on("receive_priv_message", (data) => {
-      if (data.from.id !== uniqueConversationID) {
-        console.log("non-message")
+      console.log("DATATATA: ", data)
+
+      if (data.from.id !== conversationId) { 
+        console.log("!data id: ", data)
+      // this data is logged if not directly in same room as sender
       } else {
+        console.log("ELSE data: ", data)
         setChat([...chat, { id: data.id, username: data.from.username, message: data.message }])
       }
     })
@@ -25,7 +60,7 @@ const Chat = ({ recipientInfo, socket, sender }) => {
       socket.off("receive_priv_message")
     }
   })
-  
+
   return (
     <>
     <h2>PRIVATE CHAT</h2>
