5bc48812ca999b88b2c2a41dcf1172566783e217
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-27 23:58:27 +0100

    add socket to dependency array

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,24 +5,25 @@ import { jwtDecode } from "jwt-decode"
 const Profile = () => {
   console.log("RENDER PROFILE")
 
+  const [socket] = useOutletContext()
   const token = localStorage.getItem("token")
+  console.log(token)
   const decoded = jwtDecode(token)
 
   useEffect(() => { // initiate manual connection
-    console.log("initate connection")
+    console.log("initiate connection")
     const token = localStorage.getItem("token")
     socket.auth = { token: token };
     socket.connect() 
-  })
+    
+  },[socket])
   
-  const [socket] = useOutletContext()
   const username = decoded.username
   const userId = decoded.id
 
   return (
     <>
     <h2>Welcome {username}!</h2>
-    {/* <Rooms /> */}
     <Link to="/profile/messages">Messages</Link><br />
     <Outlet context={[socket, username, userId]}/>
     </>
