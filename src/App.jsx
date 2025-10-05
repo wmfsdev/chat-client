42809b14d2b2605665720b71a6611321196a7ee6
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-10-05 23:28:50 +0100

    import loader data and create auth state for managing user authorisation status

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Link, Outlet, useNavigate, useLoaderData } from 'react-router-dom'
 import { io } from 'socket.io-client'
 
 const token = localStorage.getItem("token")
@@ -15,17 +15,24 @@ const socket = io('http://localhost:3001/profile', {
 })
 
 const App = () => {
-  
+
+  const loaderAuth = useLoaderData()
   const navigate = useNavigate()
-  const [userState, setUserState] = useState()
   const [error, setError] = useState(null)
+  const [auth, setAuth] = useState(false)
 
   useEffect(() => {
     socket.on("connect_error", err => {
-      console.log(err instanceof Error); // true
-      console.log(err.message);
-      setError(err)
-      navigate('/error')
+      if (socket.active) {
+      // temporary disconnection, the socket will automatically try to reconnect
+        console.log(err)
+        return
+      } else {
+      // the connection was forcefully closed by the server or the client itself
+        setError(err)
+        navigate('/error')
+      }
+    
     });
   })
 
@@ -35,13 +42,13 @@ const App = () => {
     <h1>Chat App</h1>
     <div className='navigation'>
       <Link to="/">Home</Link><br />
-      <Link to="profile">Profile</Link><br />
+      { auth || loaderAuth ? <><Link to="profile">Profile</Link><br /></> : null }
       <Link to="login">Login</Link><br />
       <Link to="sign-up">Signup</Link><br />
       <Link to="rooms/:id">Rooms</Link>
     </div>
     </header>
-    <Outlet context={ {socket, userState, setUserState, error} }/>
+    <Outlet context={ [socket, error, auth, setAuth] }/>
     </>
   )
 }
