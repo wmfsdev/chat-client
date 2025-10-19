c8d4614964c3c10a2d797482b57b86c769c07d05
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-10-20 00:20:35 +0100

    update emitter for 'send_priv_message' event to use acknowledgement feature

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,31 @@
 import { useEffect, useState } from "react"
 
-const Chat = ({ recipientInfo, socket, sender }) => {
+const Chat = ({ recipientInfo, socket, sender, notify }) => {
 
   const [message, setMessage] = useState('')
   const [chat, setChat] = useState([])
   const conversationId = recipientInfo.recipientId
-  
+
   function sendMessage() {
-    console.log("sending message")
     const id = crypto.randomUUID()
-    socket.emit("send_priv_message", 
-      { 
-        from: { id: sender.userId, username: sender.username }, 
-        to: conversationId, 
-        message: message, 
-        id: id
-      })
-    setChat([...chat, { id: id, username: sender.username, message: message }])
+
+    socket.emit("send_priv_message", { 
+      from: { id: sender.userId, username: sender.username }, 
+      to: conversationId, 
+      message: message, 
+      id: id
+    }, (response) => {
+      if (response.status === "Bad Request") {
+        return console.log("bad request")
+      } else {
+        setChat([...chat, { id: id, username: sender.username, message: message, timestamp: Date.now() }])
+      }
+    })
+  }
+
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp) 
+    return date.toLocaleString()
   }
 
   useEffect(() => { // RETRIEVE CHAT HISTORY
@@ -34,10 +43,11 @@ const Chat = ({ recipientInfo, socket, sender }) => {
         })
         const data = await response.json()
         const chat = data.map((d) => {
-          return { id: d.id, username: d.author.username, message: d.content }
+          return { id: d.id, username: d.author.username, message: d.content, timestamp: d.timestamp }
         })
         setChat(chat)
       } catch(err) {
+        console.log(err)
         return err
       }
     }
@@ -46,13 +56,12 @@ const Chat = ({ recipientInfo, socket, sender }) => {
   
   useEffect(() => { // RECEIVE PRIVATE MESSAGE
     socket.on("receive_priv_message", (data) => {
-      console.log("DATATATA: ", data)
 
       if (data.from.id !== conversationId) { 
         console.log("!data id: ", data)
       // this data is logged if not directly in same room as sender
       } else {
-        console.log("ELSE data: ", data)
+        notify.setNotification(data.from.username)
         setChat([...chat, { id: data.id, username: data.from.username, message: data.message }])
       }
     })
@@ -62,19 +71,18 @@ const Chat = ({ recipientInfo, socket, sender }) => {
   })
 
   return (
-    <>
-    <h2>PRIVATE CHAT</h2>
-    <p>Chatting with {recipientInfo.recipientUsername}</p>
-    <ul>
-      {chat.map(data => (
-        <li key={data.id}>{data.username}: {data.message}</li>
-      ))}
-    </ul> 
-    <div className="chat-type">
-      <input type="text" placeholder="message" onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={sendMessage}>send</button>
+    <div className="chat-messages">
+      <p>Chatting with: {recipientInfo.recipientUsername}</p>
+      <ul>
+        {chat.map(data => (
+          <li key={data.id}>{data.username}: {data.message}<p>{formatDate(data.timestamp)}</p></li>
+        ))}
+      </ul> 
+      <div className="chat-type">
+        <input type="text" onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={sendMessage}>SEND</button>
+      </div>
     </div>
-    </>
   )
 }
 
