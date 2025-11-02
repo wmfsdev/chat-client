7cfef32a60bebee3da107b2d4722fa736bc7f29f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-11-02 23:22:46 +0000

    overhaul Room component for handling public group chat for all users

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,26 +1,66 @@
-import { useOutletContext } from "react-router-dom"
-import { useEffect } from "react"
+import { useLoaderData, useOutletContext, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { jwtDecode } from "jwt-decode"
+import Chat from "./Chat"
 
 const Room = () => {
 
-  const [socket, room, username, setUsername] = useOutletContext()
+  const [socket] = useOutletContext()
+  const [users, setUsers] = useState([])
+  const [userCount, setUserCount] = useState(null)
+  const { id } = useParams()
+  const token = useLoaderData()
+  const decoded = jwtDecode(token)
+  const userId= decoded.id
+  const username = decoded.username
 
-  useEffect(() => {
-    socket.on("user_joined", (data) => {
-      console.log(data);
-      setUsername(data)
-    });
+  useEffect(() => { // REQUEST USERS - EMIT
+    socket.emit("req_users")
+  }, [socket])
+
+  useEffect(() => { // USER CONNECT
+    socket.on("user_connected", (data) => {
+      setUsers([...users, data])
+    })
     return () => {
-      socket.off("user_joined");
-    };
-  }, [setUsername, socket]);
+      socket.off("user_connected")
+    }
+  }, [users, socket])
+
+  useEffect(() => { // RECEIVE USERS - ON
+    socket.on("users", (data, userCount) => {
+      setUserCount(userCount)
+      setUsers(...users, data)
+    })
+    return () => {
+      socket.off("users")
+    }
+  }, [])
+
+  useEffect(() => { // USER DISCONNECT
+    socket.on("user_disconnected", (data) => {
+      setUsers(
+        users.filter(user => 
+          user.username !== data.username
+        )
+      )
+      setUserCount(users.length - 1)
+    })
+    return () => {
+      socket.off("user_disconnected")
+    }
+  })
 
   return (
     <>
-    <h3>{username} joined room: {room}</h3>
+    <div className="chat-frame">
+      <div className="chat-users">
+        <h3>Online: {userCount}</h3>
+      </div>
+      <Chat recipientInfo={{ recipientId: id , recipientUsername: id }} socket={socket} sender={{ username: username, userId: userId  }}/>
+    </div>
     </>
   )
-
 }
 
 export default Room
